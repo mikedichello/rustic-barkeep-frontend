984b673faa3debbb276cc17cc84af2ef42657263
mikedichello <michael.dichello@gmail.com>
2020-02-22 13:49:38 -0600

    added slider.js

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -1,2 +1,8 @@
+import React, {Component} from 'react';
 import AutoplaySlider from 'react-awesome-slider/hoc/autoply';
 import AwesomSliderStyles from 'react-awesome-slider/src/styled/fold-out-animation.scss';
+
+
+class Slider extends Component{
+    
+}
